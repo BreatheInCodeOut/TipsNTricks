a1d9a4bfba5651a4aa33ad7575bb0f0cf0151f51
Rajesh S <rajez_likes@yahoo.co.in>
2022-05-10 14:24:44 +0530

    Switch Case can be shortened further in a different way from C# 8.0

---
+++ Coding/C#/SwitchCaseShortened.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TipsAndTricks
+{
+    public class SwitchCaseShortened
+    {
+        public string GetRole()
+        {
+            int deptId = 0;
+            string role;
+
+            //switch (deptId)
+            //{
+            //    case 1:
+            //        role = "UI Dev";
+            //        break;
+            //    case 2:
+            //    case 3:
+            //        role = "Backend Dev";
+            //        break;
+            //    case 15:
+            //        role = "Q & A";
+            //        break;
+            //    default:
+            //        role = "Not Assigned";
+            //        break;
+            //}
+
+            //Available from C# 8.0
+
+            role = deptId switch
+            {
+                1 => "UI Dev",
+                var n when 
+                    n == 2 || n == 3 => "Backend Dev",
+                15 => "Q & A",
+                _ => "Not Assigned"
+            };
+
+            return role;
+        }
+    }
+}
